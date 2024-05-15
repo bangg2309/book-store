18181802afe3bd1f0328e48577246fbafcc3da51
bangg2309 <tuyeu1aido@gmail.com>
2024-05-15 14:28:25 +0700

    create: initial project using spring boot

---
+++ book-store-server/src/test/java/org/example/bookstoreserver/BookStoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookstoreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
