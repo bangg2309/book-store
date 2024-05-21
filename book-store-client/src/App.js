6710db3a37f008564ba58d1fb03d921c6f6ffe22
bangg2309 <tuyeu1aido@gmail.com>
2024-05-21 18:26:48 +0700

    update: home page

--- book-store-client/src/App.js
+++ book-store-client/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import './index.css';
 import Router from "./router/Router";
-
+import {ToastContainer} from "react-toastify";
 function App() {
-  return (
-      <div className="App">
-        <Router></Router>
-      </div>
-  );
+    return (
+        <div className="App">
+            <ToastContainer/>
+            <Router></Router>
+        </div>
+    );
 }
 
 export default App;
