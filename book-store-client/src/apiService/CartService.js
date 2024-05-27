e8310eb95136c7192205117acdc993e216611c68
bangg2309 <tuyeu1aido@gmail.com>
2024-05-27 23:16:12 +0700

    impl: handler logout

--- book-store-client/src/apiService/CartService.js
+++ book-store-client/src/apiService/CartService.js
@@ -1,5 +1,8 @@
 import * as publicRequest from '../utils/requestConfig';
 
+export const getCart = async () => {
+    return await publicRequest.get('carts');
+}
 export const addToCart = async (data) => {
     return await publicRequest.post('carts', data);
 }
